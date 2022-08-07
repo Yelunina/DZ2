0f9e555638dc99784dc869a1cd8dee05c55ff51a
IRYNA YELUNINA <lucky.asterisk@gmail.com>
2022-08-07 20:11:42 +0300

    load complementation
    DZ2

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+let admin;
+let name = 'Джон';
+admin = name;
+alert(admin);
+let ourPlanetName = 'Земля';
+let currentUserName = 'Джон';
+let number = '10'+ 10; // это все ещё строка (string)
+typeof(number);
+let Number ='10'; // и это все ещё строка (string)
+typeof (Number);
+let myNumber = 500; // теперь это число (number)
+typeof(myNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
